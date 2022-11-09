127bb7323adb0c530026d582ad4c0b7b82dac7a8
Professor Rice <george.rice@uta.edu>
2022-11-09 16:13:09 -0600

    22 code

---
+++ 22/code_from_slides/ImplementsComparable.java
@@ -0,0 +1,38 @@
+import java.util.Arrays;
+
+class WithInteger implements Comparable<Integer> {
+    public WithInteger(int value) {
+        this.value = value;
+    }
+    @Override
+    public int compareTo(Integer rhs) {
+        return value - rhs;
+    }
+    private int value;
+}
+
+class WithString implements Comparable<String> {
+    public WithString(Integer value) {
+        this.value = value;
+    }
+    @Override
+    public int compareTo(String rhs) {
+        return value - Integer.parseInt(rhs);
+    }
+    private Integer value;
+}
+
+
+
+public class ImplementsComparable {
+    public static void main(String[] args) {
+        WithInteger wi = new WithInteger(2);
+        for(int i=0; i<5; ++i) 
+            System.out.println("For " + i + ", compareTo = " + wi.compareTo(i));
+            
+        WithString ws = new WithString(2);
+        for(String s : Arrays.asList("1", "2", "3")) 
+            System.out.println("For \"" + s + "\", compareTo = " + ws.compareTo(s));
+    }
+}
+
